d492bbb1d26815a6b1e93815ec1a3f62d7e9cc20
Christopher Tran <christopher.long.tran@gmail.com>
2017-12-20 11:41:51 -0500

    added and made sure handlebars was working, set up body parser.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const app = express()
 
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('yay')
+  res.render('welcome')
+})
+
+app.post('/', (req, res) => {
+  res.send(`${req.body.recipe_name} ${req.body.recipe_description} ${req.body.recipe_instructions} ${req.body.recipe_ingredients}`)
 })
 
 app.listen(4000, () => {
