387cf13a5d48e931a34b9827e6bf8f1f17c59497
Natalee C <COPENATALEE@ME.COM>
2017-12-20 19:21:25 -0500

    added more code to index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const hbs = require('hbs')
+const hbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const recipes = require('./controllers/recipes.js')
 
@@ -9,12 +9,28 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.set('view engine', 'hbs')
 
-app.engine()
+app.engine(
+  '.hbs', 
+  hbs({
+    extname: ".hbs",
+    partialsDir: "views/",
+    layoutsDir: "views/",
+    defaultLayout: "layout-main"
+  })
+)
 
 
 app.get('/', (req, res) => {
   res.render('./welcome')
 })
+
+app.get("/:name", (req, res) => {
+  //nedd info
+})
+
+
+app.use("/recipes", recipes)
+
 
 app.post('/', (req, res) => {
   res.send(`${req.body.recipe_name} ${req.body.recipe_description} ${req.body.recipe_instructions} ${req.body.recipe_ingredients}`)
