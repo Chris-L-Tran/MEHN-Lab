ec161a395fdfb8e2657b3c963f9d2eb68bd28869
Christopher Tran <christopher.long.tran@gmail.com>
2018-01-03 12:49:45 -0500

    syntax corrections

--- config/passport.js
+++ config/passport.js
@@ -1,63 +1,59 @@
-var LocalStrategy = require('passport-local').Strategy;
-var User = require('../models/user');
+var LocalStrategy = require('passport-local').Strategy
+var User = require('../models/user')
 
-module.exports = function(passport) {
-  passport.serializeUser(function(user, done) {
-    done(null, user.id);
-  });
+module.exports = function (passport) {
+  passport.serializeUser(function (user, done) {
+    done(null, user.id)
+  })
 
-  passport.deserializeUser(function(id, callback) {
-    User.findById(id, function(err, user) {
-      callback(err, user);
-    });
-  });
+  passport.deserializeUser(function (id, callback) {
+    User.findById(id, function (err, user) {
+      callback(err, user)
+    })
+  })
 
   passport.use('local-signup', new LocalStrategy({
-    usernameField : 'email',
-    passwordField : 'password',
-    passReqToCallback : true
-  }, function(req, email, password, callback) {
-
-    User.findOne({ 'local.email' :  email }, function(err, user) {
-      if (err) return callback(err);
-
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  }, function (req, email, password, callback) {
+    User.findOne({'local.email': email}, function (err, user) {
+      if (err) return callback(err)
 
       if (user) {
-        return callback(null, false, req.flash('signupMessage', 'This email is already used.'));
+        return callback(null, false, req.flash('signupMessage', 'This email is already used.'))
       } else {
+        var newUser = new User()
+        newUser.local.email = email
+        newUser.local.password = newUser.encrypt(password)
 
-        var newUser = new User();
-        newUser.local.email = email;
-        newUser.local.password = newUser.encrypt(password);
-
-        newUser.save(function(err) {
-          if (err) throw err;
-          return callback(null, newUser);
-        });
+        newUser.save(function (err) {
+          if (err) throw err
+          return callback(null, newUser)
+        })
       }
-    });
-  }));
+    })
+  }))
 
   passport.use('local-login', new LocalStrategy({
-    usernameField : 'email',
-    passwordField : 'password',
-    passReqToCallback : true
-  }, function(req, email, password, callback) {
-
-    User.findOne({ 'local.email' :  email }, function(err, user) {
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  }, function (req, email, password, callback) {
+    User.findOne({'local.email': email}, function (err, user) {
       if (err) {
-        return callback(err);
+        return callback(err)
       }
 
       if (!user) {
-        return callback(null, false, req.flash('loginMessage', 'No user found.'));
+        return callback(null, false, req.flash('loginMessage', 'No user found.'))
       }
 
       if (!user.validPassword(password)) {
-        return callback(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+        return callback(null, false, req.flash('loginMessage', 'Oops! Wrong password.'))
       }
 
-      return callback(null, user);
-    });
-  }));
-};
+      return callback(null, user)
+    })
+  }))
+}
--- config/routes.js
+++ config/routes.js
@@ -1,30 +1,29 @@
-var express = require('express');
-var router = express.Router();
-var usersController = require('../controllers/users');
-var staticsController = require('../controllers/statics');
+var express = require('express')
+var router = express.Router()
+var usersController = require('../controllers/users')
+var staticsController = require('../controllers/statics')
 
-function authenticatedUser(req, res, next) {
+function authenticatedUser (req, res, next) {
+  if (req.isAuthenticated()) return next()
 
-  if (req.isAuthenticated()) return next();
-
-  res.redirect('/');
+  res.redirect('/')
 }
 
 router.route('/')
-  .get(staticsController.home);
+  .get(staticsController.home)
 
 router.route('/signup')
   .get(usersController.getSignup)
-  .post(usersController.postSignup);
+  .post(usersController.postSignup)
 
 router.route('/login')
   .get(usersController.getLogin)
-  .post(usersController.postLogin);
+  .post(usersController.postLogin)
 
-router.route("/logout")
-  .get(usersController.getLogout);
+router.route('/logout')
+  .get(usersController.getLogout)
 
-router.route("/secret")
-   .get(authenticatedUser, usersController.secret);
+router.route('/secret')
+   .get(authenticatedUser, usersController.secret)
 
-module.exports = router;
+module.exports = router
